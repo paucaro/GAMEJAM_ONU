ee686bfb1c3d46474b333e2e3b2775d6cd033d7a
paucaro <carolinaasto@gmail.com>
2020-12-08 18:14:48 -0500

    Fixing map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Map : MonoBehaviour
 {
     // Test
-    float seconds = 10;
-    float timer;
+    //float seconds = 10;
+    //float timer;
 
     // Variables
     public float percent;
@@ -16,22 +16,35 @@ public class Map : MonoBehaviour
     Vector3 finalPos;
     Vector3 diferencia;
 
+    Vector3 player;
+    Vector3 home;
+
     void Start()
     {
         start = transform.localPosition;
         startPos = GameObject.FindGameObjectWithTag("inicioMap").transform.localPosition;
         finalPos = GameObject.FindGameObjectWithTag("finalMap").transform.localPosition;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform.localPosition;
+        home = GameObject.FindGameObjectWithTag("home").transform.localPosition;
         diferencia = finalPos - startPos;
     }
 
     void Update()
     {
-        if (timer <= seconds) {
+        /*if (timer <= seconds) {
             // Test
             timer += Time.deltaTime;
             percent = timer / seconds;
             // Magic logic
             transform.localPosition = start + diferencia * percent;
-        }
+        }*/
+
+        Vector3 actualPlayerPosition = GameObject.FindGameObjectWithTag("Player").transform.localPosition;
+        percent = (home.x - actualPlayerPosition.x) / (home.x - player.x);
+        percent = 1 - percent;
+        Debug.Log("percent" + percent);
+
+        transform.localPosition = start + diferencia * percent;
     }
 }
