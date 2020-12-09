88a27597928218420f66432940d635b71ae80ac8
paucaro <carolinaasto@gmail.com>
2020-12-08 20:03:39 -0500

    Adding sound button inicio

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public AudioSource fuente;
+    public AudioClip clip;
+
+    void Start()
+    {
+        fuente.clip = clip;
+    }
+
+    public void Reproducir() {
+        fuente.Play();
+    }
+}
